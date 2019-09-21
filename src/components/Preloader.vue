6ed91e86e1315d7c4f918d0faa5eff591fc3b525
tardigrada777 <ntwrite@gmail.com>
2019-09-21 11:39:58 +0300

    Фикс с загрузкой видео, чтобы на всех браузерах грузился прелоадер

--- src/components/Preloader.vue
+++ src/components/Preloader.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="preloader">
-    <video
-      autoplay
-      loop
-      src="https://r6---sn-axq7sn7z.c.drive.google.com/videoplayback?id=53a2b6c98c2e9dc4&itag=59&source=webdrive&requiressl=yes&mm=30&mn=sn-axq7sn7z&ms=nxu&mv=m&mvi=5&nh=IgpwcjAyLmxlZDAzKgkxMjcuMC4wLjE&pl=24&sc=yes&ttl=transient&ei=i9yFXeDtJtqw8wTekLM4&susc=dr&driveid=1nw0HsV4oELVmxgARwVuzqowIsv882qQD&app=texmex&mime=video/mp4&dur=23.986&lmt=1568818890718246&mt=1569053732&ip=178.67.161.46&ipbits=0&expire=1569068235&cp=QVNLQUFfV1JRRlhOOkRYYzNfMUxqMjZo&sparams=ip,ipbits,expire,id,itag,source,requiressl,mm,mn,ms,mv,mvi,nh,pl,sc,ttl,ei,susc,driveid,app,mime,dur,lmt,cp&signature=D3E39F14CB8D4BA044D97D17701A302A5D4716CEC2CE0457E10FD5DC0BC74FBE.37DB6054E87A562E869B185C03243BA773D1A6B82FEBBAEAAAF14FAA8A42EC40&key=us0&cpn=_03vnfukHXA4MYb2&c=WEB_EMBEDDED_PLAYER&cver=20190920"
-    ></video>
+    <video onloadeddata="this.play();" autoplay loop>
+      <source type="video/mp4" src="../assets/video/videoplayback.mp4" />Your browser does not support the video tag or the file format of this video.
+    </video>
   </div>
 </template>
 
